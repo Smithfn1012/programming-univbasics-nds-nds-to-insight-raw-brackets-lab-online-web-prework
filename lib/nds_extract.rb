4e488f55f8213005c6dfe8800f977d958908c22f
Felicia Smith <smithfn1012@gmail.com>
2019-11-25 22:59:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,3 +26,15 @@ def directors_totals(nds)
   end
   result
 end
+
+def gross_for_director(director_data)
+  movie_index = 0
+  movies = director_data[:movies]
+  total_gross_for_director = 0
+  
+  while movie_index < movies.length do
+    total_gross_for_director += movies[movie_index][:worldwide_gross]
+    movie_index += 1
+  end
+  total_gross_for_director
+end
